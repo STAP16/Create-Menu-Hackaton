c5e88fa942091239539c3103217ee78c10fb1cfa
Maria Soboleva <marymyasnikova501@gmail.com>
2025-11-28 22:23:52 +0400

    Тестовый функционал по изменению фона страницы

--- src/app.js
+++ src/app.js
@@ -1,2 +1,5 @@
 import './styles.css'
+import {BackgroundModule} from './modules/background.module'
 
+ const backgroundModule = new BackgroundModule()
+backgroundModule.trigger();
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,18 @@
 import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
-
+    constructor(){
+        super('backgroundColor', 'Рандомный цвет фона');
+    }
+    trigger (){
+        console.log('t');
+        const rowCode = '0123456789ABCDEF';
+        let color = '#';
+        for (let i=0; i<6; i+=1){
+            const idRow = Math.random() * rowCode.length;
+            const floorIdRow = Math.floor(idRow);
+            color = color + rowCode[floorIdRow];
+        }
+    document.body.style.backgroundColor = color;
+}
 }
