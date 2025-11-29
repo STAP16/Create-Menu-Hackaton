71fdeb2baa3e07e471456d5f63738ac5940fcc76
Степан <stepancat61@gmail.com>
2025-11-29 20:05:45 +0300

    revert commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,2 @@
-import "./styles.css";
-import { ContextMenu } from "./menu";
-import { TestModule } from "./modules/test.module";
-import { SoundModule } from "./modules/sound.module";
-import {AstroModule} from "./modules/astro.module";
+import './styles.css'
 
-const contextMenu = new ContextMenu("#menu");
-
-const testModule = new TestModule("test", "Тестовый блок");
-const soundModule = new SoundModule("sound", "Рандомный звук");
-const astroModule = new AstroModule ("astroSign", "Астрологический прогноз");
-
-
-contextMenu.add(testModule);
-contextMenu.add(soundModule);
-contextMenu.add(astroModule);
