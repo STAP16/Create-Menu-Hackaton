b280e151a3e0e057eba645886f1d3bb3083dbee6
LydiaBondareva <lidiabondareva08@gmail.com>
2025-11-29 09:14:01 +0500

    audio

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,7 +1,16 @@
 import {Module} from '../core/module'
 
 export class SoundModule extends Module {
+    constructor(type, text) {
+    super(type, text);
+    this.type = type;
+    this.text = text;
+  }
 
+  trigger(event) {
+   let sounds = ['https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg']
+ }
 }
 
-console.log('sound')
+var audio = new Audio('https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg');
+audio.play();
