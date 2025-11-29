5c30edcd3381cc8a7a621b3c8e855d8124198a47
Степан <stepancat61@gmail.com>
2025-11-29 19:51:07 +0300

    add astro sign

--- src/app.js
+++ src/app.js
@@ -5,13 +5,16 @@ import "./styles.css";
 import { ContextMenu } from "./menu";
 import { TestModule } from "./modules/test.module";
 import { SoundModule } from "./modules/sound.module";
+import {AstroModule} from "./modules/astro.module";
 
 const contextMenu = new ContextMenu("#menu");
 
 const testModule = new TestModule("test", "Тестовый блок");
 const backgroundColorModule = new BackgroundModule("color", "Поменять цвет");
 const soundModule = new SoundModule("sound", "Рандомный звук");
+const astroModule = new AstroModule ("astroSign", "Астрологический прогноз");
 
 contextMenu.add(testModule);
 contextMenu.add(backgroundColorModule);
 contextMenu.add(soundModule);
+contextMenu.add(astroModule);
