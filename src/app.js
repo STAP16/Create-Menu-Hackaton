5b65be499a66c6a7a0651b2c761f75a5246bfb75
Степан <stepancat61@gmail.com>
2025-11-29 20:30:23 +0300

    fixed

--- src/app.js
+++ src/app.js
@@ -5,22 +5,17 @@ import "./styles.css";
 import { ContextMenu } from "./menu";
 import { TestModule } from "./modules/test.module";
 import { SoundModule } from "./modules/sound.module";
-<<<<<<< HEAD
-import {ClicksModule} from './modules/clicks.module';
-=======
-import { AstroModule } from "./modules/astro.module";
->>>>>>> 024d339ff240a962beedfc9da90dd8566de19b1a
+import { ClicksModule } from "./modules/clicks.module";
 import { TestButton } from "./buttons/test_button";
+import { AstroModule } from "./modules/astro.module";
 
 const contextMenu = new ContextMenu("#menu");
 
 const testModule = new TestModule("test", "Тестовый блок");
 const backgroundColorModule = new BackgroundModule("color", "Поменять цвет");
 const soundModule = new SoundModule("sound", "Рандомный звук");
-<<<<<<< HEAD
 const clicksModule = new ClicksModule("clicks", "test click");
 
-=======
 const astroModule = new AstroModule("astroSign", "Астрологический прогноз");
 
 const testBtn = new TestButton("Test", 0, 0);
@@ -31,4 +26,3 @@ contextMenu.add(backgroundColorModule);
 contextMenu.add(soundModule);
 contextMenu.add(astroModule);
 contextMenu.add(clicksModule);
->>>>>>> 024d339ff240a962beedfc9da90dd8566de19b1a
