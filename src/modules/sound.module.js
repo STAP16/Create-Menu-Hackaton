4b11e1f0962772a4e812df8e076aec5f203f5ce3
LydiaBondareva <lidiabondareva08@gmail.com>
2025-11-29 09:31:00 +0500

    arrtry

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -8,11 +8,16 @@ export class SoundModule extends Module {
   }
 
   trigger(event) {
-   let sounds = ['https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg']
+   
  }
 }
 document.body.addEventListener('click', ()=>{
-
+    let sounds = ['https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg', 'https://commondatastorage.googleapis.com/codeskulptor-assets/jump.ogg', 
+    'https://codeskulptor-demos.commondatastorage.googleapis.com/descent/background%20music.mp3', 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
+    'https://codeskulptor-demos.commondatastorage.googleapis.com/pang/arrow.mp3', 'https://commondatastorage.googleapis.com/codeskulptor-demos/pyman_assets/theygotcha.ogg'
+   ]
+const randomSound = Math.floor(Math.random()*(sounds.length-1))
+console.log(randomSound)
 var audio = new Audio('https://commondatastorage.googleapis.com/codeskulptor-assets/Evillaugh.ogg');
 audio.play();
 })
