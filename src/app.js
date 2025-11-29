1711d4aaa9333fa9f04206c9c74c9d6120c92288
Krutikhin Ivan <krutikhinivan@gmail.com>
2025-11-29 19:18:03 +0300

    create module clicks.module v0.1.0 from Ivan

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ import "./styles.css";
 import { ContextMenu } from "./menu";
 import { TestModule } from "./modules/test.module";
 import { SoundModule } from "./modules/sound.module";
+import {ClicksModule} from './modules/clicks.module';
 
 const contextMenu = new ContextMenu("#menu");
 
 const testModule = new TestModule("test", "Тестовый блок");
 const backgroundColorModule = new BackgroundModule("color", "Поменять цвет");
 const soundModule = new SoundModule("sound", "Рандомный звук");
-
+const clicksModule = new ClicksModule("clicks", "test click");
 
 contextMenu.add(testModule);
 contextMenu.add(soundModule);
-contextMenu.add(backgroundColorModule);
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,3 +1,38 @@
 import { Module } from "../core/module";
 
-export class ClicksModule extends Module {}
+export class ClicksModule extends Module {
+
+    constructor(type, text) {
+        super(type, text);
+        this.counterClicks = 0;
+        this.counterStatus = true;
+
+    }
+
+    trigger() {
+        setTimeout(() => {            
+            this.counterStatus = false;            
+            this.renderCounterClicks(this.counterClicks)
+            this.counterClicks = 0;
+        }, 3000);
+
+
+        document.body.addEventListener('click', (event) => {
+            if (event && this.counterStatus) {
+                this.counterClicks += 1;                
+            }
+        });
+
+    }
+
+    renderCounterClicks(counterClick) {
+        const containerForCounter = document.createElement('div');
+        containerForCounter.classList.add('containerForCounter');
+        containerForCounter.style.backgroundColor = 'skyblue';
+        containerForCounter.style.display = 'inline-block';
+        containerForCounter.style.padding = '10px';
+        containerForCounter.style.borderRadius = "3px"
+        containerForCounter.textContent = `Количество кликов за 3 секунды ${counterClick}`;
+        document.body.append(containerForCounter);
+    };
+}
