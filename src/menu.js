c52f24e84c7bbc4e27d68824edcf3f89c9e820a7
Степан <stepancat61@gmail.com>
2025-11-30 18:00:51 +0300

    fix openX position

--- src/menu.js
+++ src/menu.js
@@ -3,10 +3,26 @@ import { Menu } from "./core/menu";
 export class ContextMenu extends Menu {
   constructor(selector) {
     super(selector);
+
     document.body.addEventListener("contextmenu", (event) => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
       event.preventDefault();
-      const openX = event.clientX;
-      const openY = event.clientY;
+      let openX = event.clientX;
+      let openY = event.clientY;
+
+      if (openX > width - 100) {
+        openX -= 140;
+      }
+      if (openX < 0) {
+        openX += 140;
+      }
+      if (openY > width - 240) {
+        console.log(openY);
+        console.log(width);
+        //Вот тут разберись пж, я вообще не понимаю,
+        //Нужно сделать так, что если часть меню как бы уреазается, оно должно на опрределное кол-во пиксилей вверх идти
+      }
 
       (this.el.style.left = `${openX}px`),
         (this.el.style.top = `${openY}px`),
